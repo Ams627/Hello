f1c539ce52cfd1dddb68cd57c7c71fcf8387dc2f
Adrian Sims <adriansims62781@gmail.com>
2022-11-16 12:27:44 +0100

    updates

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.CodeDom;
+
+
+//this is a test
+
+if(1==1) Console.WriteLine("Hello, World!");
+
+var str = "Hello";
+ReadOnlySpan<char> slice = str[1..2];
+
+public enum C {
+    Dog=1,
+    Cat=2
+}
+
